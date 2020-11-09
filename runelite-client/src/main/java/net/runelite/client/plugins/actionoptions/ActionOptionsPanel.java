41dc9409d353a5c2aca4a00fe19a93a05caf3e91
Pyrnapp-Laptop <49798254+Pyrnapp@users.noreply.github.com>
2020-11-09 18:29:52 -0500

    Created Classes for Action Options - Plugin class for data, panel for display

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/actionoptions/ActionOptionsPanel.java
@@ -0,0 +1,9 @@
+package net.runelite.client.plugins.actionoptions;
+
+//Imports
+import net.runelite.client.ui.PluginPanel;
+
+
+public class ActionOptionsPanel extends PluginPanel{
+
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/actionoptions/ActionOptionsPlugin.java
@@ -0,0 +1,5 @@
+package net.runelite.client.plugins.actionoptions;
+
+public class ActionOptionsPlugin {
+
+}
