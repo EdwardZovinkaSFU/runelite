0a3189c93936f8a01da19497ea5afc6b91eef0fd
Pyrnapp-Laptop <49798254+Pyrnapp@users.noreply.github.com>
2020-11-09 19:21:40 -0500

    Created Panel Bootstrapper based on https://github.com/osrsbox/runelite/blob/master/runelite-client/src/main/java/net/runelite/client/plugins/hiscore/HiscorePlugin.java

--- runelite-client/src/main/java/net/runelite/client/plugins/actionoptions/ActionOptionsPanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/actionoptions/ActionOptionsPanel.java
@@ -1,9 +1,18 @@
 package net.runelite.client.plugins.actionoptions;
 
 //Imports
+import net.runelite.client.ui.ColorScheme;
 import net.runelite.client.ui.PluginPanel;
+import net.runelite.api.events.ItemSpawned;
+import net.runelite.api.events.ItemDespawned;
 
+import javax.inject.Inject;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
 public class ActionOptionsPanel extends PluginPanel{
+
+
+
 
 }
--- runelite-client/src/main/java/net/runelite/client/plugins/actionoptions/ActionOptionsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/actionoptions/ActionOptionsPlugin.java
@@ -1,5 +1,62 @@
 package net.runelite.client.plugins.actionoptions;
 
-public class ActionOptionsPlugin {
+//Imports
+import net.runelite.api.Client;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.ClientToolbar;
+import net.runelite.client.ui.NavigationButton;
+import net.runelite.client.util.ImageUtil;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+import java.awt.image.BufferedImage;
+
+@PluginDescriptor(
+        name = "ActionOptions",
+        description = "Lists the number of interactable entities and NPCs in the rendered area." +
+                "To Do: List of actions available in the rendered area.",
+        tags = {"panel","actions"},
+        loadWhenOutdated = true
+)
+
+public class ActionOptionsPlugin extends Plugin{
+
+    @Inject
+    @Nullable
+    private Client client;
+
+    @Inject
+    private ClientToolbar clientToolbar;
+
+    private NavigationButton navButton;
+    private ActionOptionsPanel actionoptionspanel;
+
+    @Override
+    protected void startUp() throws Exception {
+
+        final BufferedImage icon = ImageUtil.getResourceStreamFromClass(getClass(), "normal.png");
+
+        navButton = NavigationButton.builder()
+                .tooltip("ActionOptions")
+                .icon(icon)
+                .priority(5)
+                .panel(actionoptionspanel)
+                .build();
+
+        clientToolbar.addNavigation(navButton);
+
+    }
+
+    @Override
+    protected void shutDown() throws Exception{
+        clientToolbar.removeNavigation(navButton);
+    }
+
+
+
+
+
 
 }
