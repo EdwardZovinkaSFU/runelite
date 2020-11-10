e83021d915ac57eb37d865317909e1d873b1b34a
Pyrnapp-Laptop <49798254+Pyrnapp@users.noreply.github.com>
2020-11-10 15:32:35 -0500

    Removed Unecessary JPanels from Infopanel, created placeholders for counters. Began writing eventlistener.

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/actionoptions/CounterListener.java
@@ -0,0 +1,11 @@
+package net.runelite.client.plugins.actionoptions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CounterListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
